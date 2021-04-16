1d710229f9126e409248996aa6e630bda3569ab7
th3sp1s <69475918+th3sp1s@users.noreply.github.com>
2021-04-16 11:40:38 +0200

    Update yaralizer.sh

--- yaralizer.sh
+++ yaralizer.sh
@@ -5,12 +5,17 @@
 #get the input name until it finds a "."
 filename="${1//.*}_yaralized.yar"
 
+#remove all empty lines or only-white-spaces lines
+sed -i '/^[[:space:]]*$/d' $1
+
+
 echo "rule ${1//.*}rule" > $filename
 echo "{" >> $filename
 echo -e "\tstrings:" >> $filename
 
 i=0
 
+#reads from input file because <$1
 while read line
 do
 	echo -e "\t\t\$st$i = \"$line\" ascii wide" >> $filename
@@ -18,7 +23,15 @@ do
 done < $1
 
 echo -e "\tcondition:" >> $filename
-echo -e "\t\tall of them" >> $filename
+
+
+if [ "$2" = "-any" ] ; then
+	echo -e "\t\tany of them" >> $filename
+
+else 
+	echo -e "\t\tall of them" >> $filename
+fi
+
 echo "}" >> $filename
 
-echo "Successfully created $filename"
+echo "Created file $filename"
